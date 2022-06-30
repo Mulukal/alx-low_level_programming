c27dfec9fe8573deb069039e09c671df8170fdca
Mulukal <mulukalgemechu@gmail.com>
2022-06-30 15:57:17 -0700

    c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** 
+ * main-prints the prase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited propery, non-zero otherwise
+ */
+int main (void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
