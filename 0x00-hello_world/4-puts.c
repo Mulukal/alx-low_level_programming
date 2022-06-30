9d343afd7a9963e4564c769e41f8367f809ce077
Mulukal <mulukalgemechu@gmail.com>
2022-06-30 16:10:12 -0700

    c command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /** 
  * main-prints the prase "Programming is like building a multilingual puzzle
- * Return: 0 if exited propery, non-zero otherwise
+ * Return: 0 if exited propery, 
  *
- * * /
- 
-    int main (void)
-       {      
- 
-                 puts("\"Programming is like building a multilingual puzzle");
+ */
+int main(void)
 
-			return (0);
-	}
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+						return (0);
+
+}
