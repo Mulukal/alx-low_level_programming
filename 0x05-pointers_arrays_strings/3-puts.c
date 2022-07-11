4ac5d8c2946826b654e8d942dfa2b60b5a3abc94
Mulukal <mulukalgemechu@gmail.com>
2022-07-11 12:54:47 -0700

    task 03

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,29 @@
+#include "main1.h"
+
+/**
+ *_puts - print a string
+ *@str:pointer char
+ *return:void
+ */
+
+void _puts(char *str)
+
+{
+
+		int i;
+
+
+
+			for (i = 0; str[i] != '\0'; i++)
+
+					{
+
+								_putchar(str[i]);
+
+									}
+
+			_putchar('\n');
+
+			return;
+
+}
