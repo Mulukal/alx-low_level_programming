dd20c74a2afa8aff0053cc98994814596d72166b
Mulukal <mulukalgemechu@gmail.com>
2022-07-11 13:46:38 -0700

    t4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,14 +3,10 @@
 
 
 /**
- *
- *  * print_rev - print a string
- *
- *   * @s:pointer char
- *
- *    * return:void
- *
- *     */
+ *print_rev - print a string
+ *@s:pointer char
+ * return:void
+ */
 
 void print_rev(char *s)
 
