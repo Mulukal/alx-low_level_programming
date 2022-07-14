f61d309c4551bd697390f6f1a6485e6aeb98594b
Mulukal <mulukalgemechu@gmail.com>
2022-07-14 14:21:10 -0700

    task 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,18 +3,11 @@
 
 
 /**
- *
- *  * _strcmp - function that compares two strings.
- *
- *   *
- *
- *    * @s1: pointer to char source 1
- *
- *     * @s2: pointer to char source 2
- *
- *      * Return: int
- *
- *       */
+ * _strcmp - function that compares two strings.
+ *  @s1: pointer to char source 1
+ * @s2: pointer to char source 2
+ * Return: int
+ */
 
 
 
